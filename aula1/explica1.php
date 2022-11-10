c3f7235ab2832b7a3770875270b5b48817166443
antunespk <antunes.patrick@hotmail.com>
2022-11-09 21:32:45 -0300

    exercicios

---
+++ aula1/explica1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Aula 1 do php iniciada</h1>
+    <?php //comando de entrada do php
+
+    $variavel = "criandoUmaVariavel"; // " pois é uma string
+    
+    $nome = "Patrick";
+    //numero inteiro
+    $idade = 32;
+    
+    // real float
+    $salario = 2700;
+
+    //mostra na tela
+    echo "O nome do aluno é: " $nome "<br>" "A idade do aluno é: " .$idade "<br>" "A renda é: " .$salario; 
+
+?>
+</body>
+</html>
